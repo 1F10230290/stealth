9ee7e0cb7c5f095e557e4f1b02e6effb5911e9f9
c0a23046 <syunkun1973@gmail.com>
2026-01-29 17:46:26 +0900

    キャラクターの移動の調整

--- Assets/Syun_Script/Player_Move.cs
+++ Assets/Syun_Script/Player_Move.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
-    public float speed;
+    public Transform Camera;
+    public float PlayerSpeed;
+    public float RotationSpeed;
+
+    Vector3 speed = Vector3.zero;
+    Vector3 rot = Vector3.zero;
+
     void Start()
     {
         
@@ -12,25 +18,60 @@ public class Player_Move : MonoBehaviour
     void Update()
     {
         Move();
+        Rotation();
+
+        // カメラをプレイヤーの位置に追従させる
+        Camera.transform.position = transform.position;
     }
 
     void Move()
     {
+        speed = Vector3.zero;
+        rot = Vector3.zero;
+        
         if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0f, 0f, speed);
+            rot.y = 0;
+            MoveSet();
         }
         if(Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0f, 0f, -speed);
+            rot.y = 180;
+            MoveSet();
         }
         if(Input.GetKey(KeyCode.D))
         {
-            transform.Translate(speed, 0f, 0f);
+            rot.y = 90;
+            MoveSet();
         }
         if(Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-speed, 0f, 0f);
+            rot.y = -90;
+            MoveSet();
         }
+
+        transform.Translate(speed* Time.deltaTime);
+        
+    }
+
+    void MoveSet()
+    {
+        speed.z = PlayerSpeed;
+        transform.eulerAngles = new Vector3(0, Camera.transform.eulerAngles.y + rot.y, 0);
+    }
+
+    void Rotation()
+    {
+        float rotStep = RotationSpeed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Camera.transform.Rotate(0f, -rotStep, 0f);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Camera.transform.Rotate(0f, rotStep, 0f);
+        }
+
     }
 }
